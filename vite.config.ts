abcfacb11450e709663c3283c9671f030188ddb3
Maurice Koreman <mg.koreman99@gmail.com>
2023-02-26 16:32:33 +0100

    pwa with vite-pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "CouponMe",
+        short_name: "CouponMe",
+        description: "Coupon sharing app for couples",
+        theme_color: "#FFFFF4",
+        icons: [
+          {
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: [
       "firebase",
@@ -14,6 +47,12 @@ export default defineConfig({
     ],
   },
 });
+
+
+
+
+
+
 
 
 
