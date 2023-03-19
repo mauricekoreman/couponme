46d5a85f40b25938208b9437520e06a063a2e183
mauricekoreman <mg.koreman99@gmail.com>
2023-03-19 14:07:57 +0100

    add

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -16,4 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const messaging = getMessaging(app);
 export const auth = getAuth(app);
